fb98bf644476dc24b05d0a31b29bcd258a0363c7
adadychanka <a.dadychenko@gmail.com>
2026-03-17 12:31:52 +0100

    Add @nestjs/cqrs module to API; update package dependencies and integrate CQRS in app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { CqrsModule } from '@nestjs/cqrs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    CqrsModule.forRoot(),
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
