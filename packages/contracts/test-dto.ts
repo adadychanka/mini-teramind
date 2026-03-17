2c3aa09022fd8989e410e5a87ec961cb74217fd5
adadychanka <a.dadychenko@gmail.com>
2026-03-17 10:28:05 +0100

    init nest.js api

---
+++ packages/contracts/test-dto.ts
@@ -0,0 +1,11 @@
+export interface TestDto {
+  id: string;
+  name: string;
+  email: string;
+  phone: string;
+  address: string;
+  city: string;
+  state: string;
+  zip: string;
+  country: string;
+}
