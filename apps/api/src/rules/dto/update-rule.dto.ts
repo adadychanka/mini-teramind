981b5632840b4d3c34b6c4970ac83a051f4edb36
adadychanka <a.dadychenko@gmail.com>
2026-03-28 21:06:10 +0100

    Refactor UpdateRuleDto to omit 'id' field and streamline interface implementation. This change enhances the DTO by ensuring that the 'id' property is excluded from updates, aligning with the updated interface definition and improving type safety.

--- apps/api/src/rules/dto/update-rule.dto.ts
+++ apps/api/src/rules/dto/update-rule.dto.ts
@@ -1,15 +1,7 @@
 import { OmitType, PartialType } from '@nestjs/swagger';
 import { UpdateRuleDto as IUpdateRuleDto } from '@repo/contracts';
-import { IsNotEmpty, IsOptional, IsString, IsUUID } from 'class-validator';
 import { CreateRuleDto } from './create-rule.dto';
 
 export class UpdateRuleDto
   extends OmitType(PartialType(CreateRuleDto), ['type'])
-  implements Omit<IUpdateRuleDto, 'type' | 'severity'>
-{
-  @IsUUID()
-  @IsString()
-  @IsNotEmpty()
-  @IsOptional()
-  id?: string;
-}
+  implements Omit<IUpdateRuleDto, 'type' | 'severity' | 'id'> {}
