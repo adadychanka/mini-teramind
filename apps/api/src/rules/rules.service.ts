d800a47e020fab75c28b266ded9cc439b664a77a
adadychanka <a.dadychenko@gmail.com>
2026-03-28 20:41:10 +0100

    Add rules.mapper.ts and update RulesService to integrate rule retrieval logic
    This commit introduces a new rules.mapper.ts file that includes functions for mapping database Rule entities to RuleDto objects. Additionally, the RulesService is updated to implement the findAll method, which retrieves rules from the database using Prisma, applies the mapping, and returns paginated results. This enhancement improves the structure and functionality of rule management in the application.

--- apps/api/src/rules/rules.service.ts
+++ apps/api/src/rules/rules.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { PaginationOutputDto, RuleDto, RuleSeverity } from '@repo/contracts';
+import { Prisma } from 'generated/prisma/client';
 import { RuleType } from 'generated/prisma/enums';
+import { DEFAULT_PAGINATION_LIMIT } from 'src/common/pagination/limits';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateRuleDto } from './dto/create-rule.dto';
 import { FindRulesInputDto } from './dto/find-rules-input.dto';
 import { UpdateRuleDto } from './dto/update-rule.dto';
+import { toRuleDto } from './rules.mapper';
 
 const mockedRuleDto = {
   id: '1',
@@ -18,16 +22,41 @@ const mockedRuleDto = {
 
 @Injectable()
 export class RulesService {
+  constructor(private readonly prisma: PrismaService) {}
+
   create(createRuleDto: CreateRuleDto): Promise<RuleDto> {
     return Promise.resolve(mockedRuleDto);
   }
 
-  findAll(findRulesInputDto: FindRulesInputDto): Promise<PaginationOutputDto<RuleDto>> {
-    return Promise.resolve({
-      items: [mockedRuleDto],
-      total: 1,
-      hasNextPage: false,
-    });
+  async findAll(findRulesInputDto: FindRulesInputDto): Promise<PaginationOutputDto<RuleDto>> {
+    const { page = 1, limit = DEFAULT_PAGINATION_LIMIT } = findRulesInputDto;
+    const skip = (page - 1) * limit;
+
+    const where: Prisma.RuleWhereInput = {
+      active: findRulesInputDto.active,
+    };
+    const [rules, total] = await Promise.all([
+      this.prisma.rule.findMany({
+        skip,
+        take: limit,
+        where,
+        orderBy: {
+          createdAt: 'desc',
+        },
+      }),
+      this.prisma.rule.count({
+        where,
+      }),
+    ]);
+
+    const ruleDtos = rules.map(toRuleDto);
+    const hasNextPage = page * limit < total;
+
+    return {
+      items: ruleDtos,
+      total,
+      hasNextPage,
+    };
   }
 
   update(ruleId: string, updateRuleDto: UpdateRuleDto): Promise<RuleDto> {
